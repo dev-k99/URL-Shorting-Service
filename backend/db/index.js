cb8002c501c9fa9edd3760a96f80d36e42024c6e
dev-k99 <kwanelerh069@gmail.com>
2026-01-23 13:29:21 +0200

    "Supabase connection"

--- backend/db/index.js
+++ backend/db/index.js
@@ -4,21 +4,26 @@ import dotenv from "dotenv";
 const { Pool } = pg;
 dotenv.config();
   
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: process.env.NODE_ENV === 'production'
-//     ? { rejectUnauthorized: false }
-//     : false
-// });
-
 const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  ssl: process.env.NODE_ENV === 'production'
+    ? { rejectUnauthorized: false }
+    : false
+});
+
+/* const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   ssl: false // 🔥 THIS FIXES LOCAL ERRORS
-});
+}); */
 
 // Test database connection
 pool.on("connect", () => {
